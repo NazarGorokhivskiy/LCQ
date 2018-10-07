3fb158e7d2e30c3c645690a700cdf525f7911504
Nazar Gorokhivskiy <nazargorokhivskiy@gmail.com>
2018-10-08 00:04:31 +0300

    added google map

--- app/src/main/kotlin/ua/lviv/iot/MainActivity.kt
+++ app/src/main/kotlin/ua/lviv/iot/MainActivity.kt
@@ -1,12 +1,61 @@
 package ua.lviv.iot
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import ua.lviv.iot.utils.*
+import android.content.res.Resources
+import android.util.Log
+import com.google.android.gms.maps.model.MapStyleOptions
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val mTAG = "MainActivity"
+
+    private lateinit var mMapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mMapView = findViewById(R.id.mapView)
+        mMapView.onCreate(savedInstanceState)
+        mMapView.onResume()
+        try {
+            MapsInitializer.initialize(applicationContext)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        mMapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        if (googleMap == null) {
+            Log.e(mTAG, "Map is not initialized")
+            return
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            googleMap.isMyLocationEnabled = true
+        }
+
+        // Customise the map style
+        try {
+            val success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style_json))
+            if (!success) Log.e(mTAG, "Style parsing failed.")
+
+        } catch (e: Resources.NotFoundException) {
+            Log.e(mTAG, "Can't find style. Error: ", e)
+        }
+
+        // Moving camera to Lviv position
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(LVIV_LAT, LVIV_LNG), 17f))
     }
 }
---
+++ app/src/main/kotlin/ua/lviv/iot/utils/Constants.kt
@@ -0,0 +1,5 @@
+package ua.lviv.iot.utils
+
+/** Longitude and latitude coordinates of Lviv */
+const val LVIV_LAT = 49.842097
+const val LVIV_LNG = 24.030536
